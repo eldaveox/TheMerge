50753224277133be3bc77cbb62f7cae08c8d1414
eldaveox <dave.rozo@live.com>
2022-03-31 14:03:10 +0200

    setup

---
+++ js/frontend.js
@@ -0,0 +1,8 @@
+function displayBands(){
+    let x = document.getElementById('allBands');
+    let y = document.getElementById('andManyMore');
+    if(x.style.display === "none"){
+        x.style.display = 'block';
+        y.style.display = 'none';
+    }    
+}
