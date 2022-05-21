ddf64196be4a2589b61209437374d02293cf97fa
eldaveox <dave.rozo@live.com>
2022-05-21 21:06:52 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,20 @@ app.use(express.static(path.join(__dirname, '../')));
 app.get('/',(req, res) => {
     res.sendFile (path.join(__dirname, '../html/index.html'))
 })
+app.get('/index.html',(req, res) => {
+    res.sendFile (path.join(__dirname, '../html/index.html'))
+})
+app.get('/tickets.html',(req, res) => {
+    res.sendFile (path.join(__dirname, '../html/tickets.html'))
+})
+app.get('/Lineup.html',(req, res) => {
+    res.sendFile (path.join(__dirname, '../html/lineup.html'))
+})
+app.get('/info.html',(req, res) => {
+    res.sendFile (path.join(__dirname, '../html/info.html'))
+})
+app.get('/login.html',(req, res) => {
+    res.sendFile (path.join(__dirname, '../html/login.html'))
+})
 
 app.listen(port, console.info(`Listening on port ${port}`))
