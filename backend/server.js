128dd64a9d66e8390fcbf0e30b7250e10e26d2bc
eldaveox <dave.rozo@live.com>
2022-05-21 20:33:20 +0200

    backend setup server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000
+
+app.use(express.static(path.join(__dirname, '../')));
+
+app.get('/',(req, res) => {
+    res.sendFile (path.join(__dirname, '../html/index.html'))
+})
+
+app.listen(port, console.info(`Listening on port ${port}`))
